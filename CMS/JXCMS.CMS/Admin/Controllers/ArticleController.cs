6d409ffe518ac0d995f586cf0d672b66481dd2dd
j4587698 <jx@jvxiang.com>
2020-01-06 06:25:03 +0800

    添加文章

---
+++ CMS/JXCMS.CMS/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,18 @@
+﻿using JXCMS.CMS.Attribute;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JXCMS.CMS.Admin.Controllers
+{
+    [Area("Admin")]
+    [AdminAuthentication]
+    public class ArticleController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            ViewBag.title = "所有文章";
+            
+            return View();
+        }
+    }
+}
---
+++ CMS/JXCMS.CMS/Entitys/ArticleEntity.cs
@@ -0,0 +1,15 @@
+﻿using FreeSql;
+using FreeSql.DataAnnotations;
+
+namespace JXCMS.CMS.Entity
+{
+    public class ArticleEntity : BaseEntity<ArticleEntity, int> 
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Column(DbType = "text")]
+        public string Content { get; set; }
+    }
+}
